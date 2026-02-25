145d2e9070bf1bff439d4805ff1d89fc2075ba80
Umar <umar@mac.home>
2026-02-25 17:33:23 +0000

    statredt c# and .net portion of project

---
+++ dotnet/FxDataIngestor/Worker.cs
@@ -0,0 +1,16 @@
+namespace FxDataIngestor;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+}
